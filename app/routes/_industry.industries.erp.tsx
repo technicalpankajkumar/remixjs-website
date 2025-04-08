a35b9fd17cf179399d1f95275e376d412a14ad8e
Pankaj Kumar <118254518+technicalpankajkumar@users.noreply.github.com>
2025-04-08 18:12:15 +0530

    Update _industry.industries.erp.tsx

--- app/routes/_industry.industries.erp.tsx
+++ app/routes/_industry.industries.erp.tsx
@@ -1,8 +1,36 @@
 
-const Index = () => {
-    return (<div>
-        Under Development
-    </div>)
-}
+import { LayoutDashboard } from "lucide-react";
+import IndustryHeroSection from "~/components/industry/IndustryHeroSection";
+import SoftwareCard from "~/components/industry/SoftwareCard";
+import { erpIndustry } from "~/constants";
 
-export default Index;
+export default function Index(){
+
+    return(<>
+    <section id="erp" className="w-full py-6 md:py-10">
+        <div className="container px-4 md:px-6 mx-auto">
+          <IndustryHeroSection
+            id="erp"
+            title="ERP Software Solutions"
+            description="Streamlining business operations with integrated software that connects all departments and functions."
+            descriptionList={erpIndustry}
+            imgSrc={"/assets/svg/erp_record.svg"}
+            icon={<LayoutDashboard className="h-6 w-6 text-primary" />}
+          />
+  
+          <div className="grid gap-6 lg:gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+            {erpIndustry.map((solution, index) => (
+              <SoftwareCard
+                key={index}
+                title={solution.title}
+                icon={<LayoutDashboard className="h-5 w-5 text-primary" />}
+                description={solution.description}
+                features={solution.features}
+                imgSrc={solution.imgSrc}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>)
+}
