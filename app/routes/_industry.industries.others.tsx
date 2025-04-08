9be22dde60d157ce55f1f24a336e0f1e09f30c58
Pankaj Kumar <118254518+technicalpankajkumar@users.noreply.github.com>
2025-04-08 18:17:37 +0530

    Update _industry.industries.others.tsx

--- app/routes/_industry.industries.others.tsx
+++ app/routes/_industry.industries.others.tsx
@@ -1,9 +1,37 @@
 
 
-const Index = () => {
-    return (<div>
-        Under Development
-    </div>)
-}
+import { Layers } from "lucide-react";
+import IndustryHeroSection from "~/components/industry/IndustryHeroSection";
+import SoftwareCard from "~/components/industry/SoftwareCard";
+import { erpIndustry } from "~/constants";
 
-export default Index;
+export default function Index(){
+
+    return(<>
+    <section id="other" className="w-full py-6 md:py-10">
+        <div className="container px-4 md:px-6 mx-auto">
+          <IndustryHeroSection
+            id="other"
+            title="Other Industry Solutions"
+            description="Specialized software solutions for diverse industries including real estate, hospitality, and agriculture."
+            descriptionList={erpIndustry}
+            imgSrc={"/assets/svg/erp_record.svg"}
+            icon={<Layers className="h-6 w-6 text-primary" />}
+          />
+  
+          <div className="grid gap-6 lg:gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+            {erpIndustry.map((solution, index) => (
+              <SoftwareCard
+                key={index}
+                title={solution.title}
+                icon={<Layers className="h-5 w-5 text-primary" />}
+                description={solution.description}
+                features={solution.features}
+                imgSrc={solution.imgSrc}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>)
+}
