8048ddfffead27813e1a3f796f48baf44e60c3ee
Pankaj Kumar <118254518+technicalpankajkumar@users.noreply.github.com>
2025-04-08 18:13:18 +0530

    Update _industry.industries.healthcare.tsx

--- app/routes/_industry.industries.healthcare.tsx
+++ app/routes/_industry.industries.healthcare.tsx
@@ -1,8 +1,38 @@
 
-const Index = () => {
-    return (<div>
-        Under Development
-    </div>)
-}
+import { Activity } from "lucide-react";
+import IndustryHeroSection from "~/components/industry/IndustryHeroSection";
+import SoftwareCard from "~/components/industry/SoftwareCard";
+import { healthIndustory } from "~/constants";
 
-export default Index;
+
+
+export default function Index(){
+
+    return(<>
+    <section id="health" className="w-full py-6 md:py-10">
+        <div className="container px-4 md:px-6 mx-auto">
+          <IndustryHeroSection
+            id="health"
+            title="Health Care Software Solutions"
+            description="Empowering healthcare providers with advanced software to improve patient care, streamline operations, and enhance medical decision-making."
+            descriptionList={healthIndustory}
+            imgSrc={"/assets/svg/hospital.svg"}
+            icon={<Activity className="h-6 w-6 text-primary" />}
+          />
+  
+          <div className="grid gap-6 lg:gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+            {healthIndustory.map((solution, index) => (
+              <SoftwareCard
+                key={index}
+                title={solution.title}
+                icon={<Activity className="h-5 w-5 text-primary" />}
+                description={solution.description}
+                features={solution.features}
+                imgSrc={solution.imgSrc}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>)
+}
